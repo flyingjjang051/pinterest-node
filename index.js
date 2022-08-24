b3253df1e3cd402265743e268f0e44b93f663094
flyingjjang051 <flyjjang051@gmail.com>
2022-08-24 11:36:23 +0900

    upload

--- index.js
+++ index.js
@@ -14,18 +14,63 @@ MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, c
   db = client.db("crudapp");
 });
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_COUND_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false })); // post에서 보낸 데이터 req.body로 받을려면 있어야함
 app.use(express.static(path.join(__dirname, "/public")));
-//app.use("/upload", express.static(path.join(__dirname, "/upload")));
+app.use("/upload", express.static(path.join(__dirname, "/upload")));
 app.set("port", process.env.PORT || 8099);
 const PORT = app.get("port");
 
+const storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, path.join(__dirname, "/upload"));
+  },
+  filename: (req, file, done) => {
+    done(null, file.originalname);
+  },
+});
+
+const fileUpload = multer({ storage: storage });
+
 app.get("/", (req, res) => {
-  res.render("index");
+  // db에서 데이터 읽어서
+  // index로 데이터 전달하기...
+  db.collection("pinterest")
+    .find()
+    .toArray((err, result) => {
+      res.render("index", { list: result });
+    });
 });
 app.get("/insert", (req, res) => {
   res.render("insert");
+});
+app.post("/register", fileUpload.single("image"), (req, res) => {
+  const title = req.body.title;
+  const date = req.body.date;
+  const category = Array.isArray(req.body.category) ? req.body.category.join(" ") : req.body.category;
+  const desc = req.body.desc;
+  const point = req.body.point;
+  const image = req.file.filename;
+  console.log(title, "===", date, "===", category, "===", desc, "===", point, "===", image);
+  //res.render("insert");
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    console.log(result);
+    db.collection("pinterest").insertOne({
+      title: title,
+      date: date,
+      category: category,
+      desc: desc,
+      point: point,
+      image: result.url,
+    });
+    res.redirect("/");
+  });
 });
 
 app.listen(PORT, () => {
